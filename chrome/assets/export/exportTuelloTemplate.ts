fc02293510dee65d9bbb536872f66dc6e4e716c4
matMatthews <amathieu64@gmail.com>
2024-02-06 23:57:32 +0100

    export lib

--- chrome/assets/export/exportTuelloTemplate.ts
+++ chrome/assets/export/exportTuelloTemplate.ts
@@ -1,195 +1,204 @@
+import tuelloJsonData from './tuello-mocks-data.json' assert {type: 'json'};
+
 ({
   tuello: function () {
+   
+    (window as any).tuelloRecords = tuelloJsonData;
     let deepMockLevel = '###IMPORT_DEEPMOCKLEVEL###';
     let tuelloFile = './###IMPORT_TUELLO_FILE###';
 
-    fetch(tuelloFile)
-      .then((response) => response.json())
-      .then((json) => {
-        (window as any).tuelloRecords = json;
-      });
-
-    document.addEventListener("DOMContentLoaded", () => {
-      (window as any).XMLHttpRequest = mockHttp.mockXHR;
-      (window as any).fetch = mockHttp.mockFetch;
-    });
-
     let compareWithMockLevel = (url1: string, url2: string): boolean => {
-      url1 = removeURLPort(url1);
-      url2 = removeURLPort(url2);
+      url1 = removeURLPortAndProtocol(url1);
+      url2 = removeURLPortAndProtocol(url2);
+      let inc = deepMockLevel;
       // @ts-ignore
-      if (deepMockLevel === 0) {
-        return new RegExp('^' + url2.replaceAll(/([.+?^=!:${}()|\[\]\/\\])/g, '\\$1').replaceAll('*', '(.*)') + '$').test(url1);
-      } else {
-        let cmp1: string;
-        let cmp2: string;
-        for (const inc of [2, 1, 0]) {
-           // @ts-ignore
-          cmp1 = url1.replace(url1.split('/', deepMockLevel + inc).join('/'), '');
-           // @ts-ignore
-          cmp2 = url2.replace(url2.split('/', deepMockLevel + inc).join('/'), '');
-          if (cmp1) {
-            break;
-          }
+      while (inc > 0) {
+        // @ts-ignore
+        url1 = url1.replace(url1.split('/', inc).join('/'), '');
+        // @ts-ignore
+        url2 = url2.replace(url2.split('/', inc).join('/'), '');
+        if (url1 && url2) {
+          break;
         }
-        return new RegExp('^' + cmp2.replaceAll(/([.+?^=!:${}()|\[\]\/\\])/g, '\\$1').replaceAll('*', '(.*)') + '$').test(cmp1);
+        // @ts-ignore
+        inc--;
       }
+      url1 = url1.substring(0, 1) === '/' ? url1.substring(1) : url1;
+      url2 = url2.substring(0, 1) === '/' ? url2.substring(1) : url2;
+      const lg1 = url1.split('/').length;
+      const lg2 = url2.split('/').length;
+      if (lg1 > lg2) {
+        url1 = url1.replace(url1.split('/', (lg1-lg2)).join('/'), '');
+      } else if (lg2 > lg1) {
+        url2 = url2.replace(url2.split('/', (lg2-lg1)).join('/'), '');
+      }
+      url1 = url1.substring(0, 1) === '/' ? url1.substring(1) : url1;
+      url2 = url2.substring(0, 1) === '/' ? url2.substring(1) : url2;
+      return new RegExp('^' + url2.replaceAll(/([.+?^=!:${}()|\[\]\/\\])/g, '\\$1').replaceAll('*', '(.*)') + '$').test(url1);
     };
 
-    const sleep = (ms: number) => {
-      const stop = new Date().getTime() + ms;
-      while (new Date().getTime() < stop) { }
-    }
+
+
+  const sleep = (ms: number) => {
+    const stop = new Date().getTime() + ms;
+    while (new Date().getTime() < stop) { }
+  }
 
 
     let mockHttp = {
-      originalXHR: window.XMLHttpRequest,
-      mockXHR() {
-        // URL avant redirect
-        let originalURL;
+    originalXHR: window.XMLHttpRequest,
+    mockXHR() {
+      // URL avant redirect
+      let originalURL;
 
-        const modifyResponse = (isOnLoad: boolean = false) => {
-          if ((window as any).tuelloRecords) {
-            // this.responseURL
-            const records = (window as any).tuelloRecords.filter(({ key, reponse, httpCode }) => compareWithMockLevel(originalURL, key));
-            if (records && records.length > 0) {
-              records.forEach(({ key, reponse, httpCode, delay }) => {
-                if (delay && isOnLoad) {
-                  sleep(delay);
-                }
-                this.responseText = JSON.stringify(reponse);
-                // Object.defineProperty(this,'responseText', JSON.stringify(reponse));
-                this.response = reponse;
-                this.status = httpCode;
-              });
-            }
-          }
-        };
-
-
-
-        const xhr = new mockHttp.originalXHR();
-        // tslint:disable-next-line:forin
-        for (const attr in xhr) {
-          if (attr === 'onreadystatechange') {
-            xhr.onreadystatechange = (...args) => {
-              if (this.readyState === 4) {
-                modifyResponse();
+      const modifyResponse = (isOnLoad: boolean = false) => {
+        if ((window as any).tuelloRecords) {
+          // this.responseURL
+          const records = (window as any).tuelloRecords.filter(({ key, reponse, httpCode }) => compareWithMockLevel(originalURL, key));
+          if (records && records.length > 0) {
+            records.forEach(({ key, reponse, httpCode, delay }) => {
+              if (delay && isOnLoad) {
+                sleep(delay);
               }
-              // tslint:disable-next-line:no-unused-expression
-              this.onreadystatechange && this.onreadystatechange.apply(this, args);
-            };
-            continue;
-          } else if (attr === 'onload') {
-            xhr.onload = (...args) => {
-              if (this.readyState === 4) {
-                modifyResponse(true);
-              }
-              this.onload && this.onload.apply(this, args);
-            };
-            continue;
-          }
-          if (typeof xhr[attr] === 'function') {
-            if (attr === 'open') {
-              const open = xhr[attr].bind(xhr);
-              this[attr] = function (method, url) {
-                originalURL = url;
-                open.call(this, method, url);
-              }
-            } else {
-              this[attr] = xhr[attr].bind(xhr);
-            }
-          } else {
-            if (attr === 'responseText' || attr === 'response' || attr === 'status') {
-              Object.defineProperty(this, attr, {
-                get: () => (this[`_${attr}`] === undefined ? xhr[attr] : this[`_${attr}`]),
-                set: val => (this[`_${attr}`] = val),
-                enumerable: true,
-              });
-            } else {
-              Object.defineProperty(this, attr, {
-                get: () => xhr[attr],
-                set: val => (xhr[attr] = val),
-                enumerable: true,
-              });
-            }
+              this.responseText = JSON.stringify(reponse);
+              // Object.defineProperty(this,'responseText', JSON.stringify(reponse));
+              this.response = reponse;
+              this.status = httpCode;
+            });
           }
         }
-      },
+      };
 
-      originalFetch: window.fetch.bind(window),
-      mockFetch: function (...args) {
-        return mockHttp.originalFetch(...args).then((response) => {
-          let txt = undefined;
-          let status = undefined;
-          if ((window as any).tuelloRecords) {
-            const records = (window as any).tuelloRecords.filter(({ key, reponse, httpCode }) => compareWithMockLevel(args[0], key));
-            if (records && records.length > 0) {
-              records.forEach(({ key, reponse, httpCode, delay }) => {
-                if (delay) {
-                  sleep(delay);
-                }
-                txt = JSON.stringify(reponse);
-                status = httpCode;
-              });
+
+      const xhr = new mockHttp.originalXHR();
+      // tslint:disable-next-line:forin
+      for (const attr in xhr) {
+        if (attr === 'onreadystatechange') {
+          xhr.onreadystatechange = (...args) => {
+            if (this.readyState === 4) {
+              modifyResponse();
             }
-          }
-
-          if (txt !== undefined) {
-            const stream = new ReadableStream({
-              start(controller) {
-                controller.enqueue(new TextEncoder().encode(txt));
-                controller.close();
-              }
-            });
-
-            const newResponse = new Response(stream, {
-              headers: response.headers,
-              status: status,
-              statusText: status,
-            });
-            const proxy = new Proxy(newResponse, {
-              get: function (target, name) {
-                switch (name) {
-                  case 'ok':
-                  case 'redirected':
-                  case 'type':
-                  case 'url':
-                  case 'useFinalURL':
-                  case 'body':
-                  case 'bodyUsed':
-                    return response[name];
-                }
-                return target[name];
-              }
-            });
-
-            for (let key in proxy) {
-              if (typeof proxy[key] === 'function') {
-                proxy[key] = proxy[key].bind(newResponse);
-              }
+            // tslint:disable-next-line:no-unused-expression
+            this.onreadystatechange && this.onreadystatechange.apply(this, args);
+          };
+          continue;
+        } else if (attr === 'onload') {
+          xhr.onload = (...args) => {
+            if (this.readyState === 4) {
+              modifyResponse(true);
             }
-
-            return proxy;
+            this.onload && this.onload.apply(this, args);
+          };
+          continue;
+        }
+        if (typeof xhr[attr] === 'function') {
+          if (attr === 'open') {
+            const open = xhr[attr].bind(xhr);
+            this[attr] = function (method, url) {
+              originalURL = url;
+              open.call(this, method, url);
+            }
           } else {
-            return response;
+            this[attr] = xhr[attr].bind(xhr);
           }
-        });
-      },
-
-    };
-
-    function removeURLPort(url: string) {
-      let ret = '';
-      try {
-        let parseURL = new URL(url);
-        parseURL.port = '';
-        ret = parseURL.toString();
-      } catch (e) {
-        ret = url;
+        } else {
+          if (attr === 'responseText' || attr === 'response' || attr === 'status') {
+            Object.defineProperty(this, attr, {
+              get: () => (this[`_${attr}`] === undefined ? xhr[attr] : this[`_${attr}`]),
+              set: val => (this[`_${attr}`] = val),
+              enumerable: true,
+            });
+          } else {
+            Object.defineProperty(this, attr, {
+              get: () => xhr[attr],
+              set: val => (xhr[attr] = val),
+              enumerable: true,
+            });
+          }
+        }
       }
-      return ret;
-    }
+    },
+
+    originalFetch: window.fetch.bind(window),
+    mockFetch: function (...args) {
+      return mockHttp.originalFetch(...args).then((response) => {
+        let txt = undefined;
+        let status = undefined;
+        if ((window as any).tuelloRecords) {
+          const records = (window as any).tuelloRecords.filter(({ key, reponse, httpCode }) => compareWithMockLevel(args[0], key));
+          if (records && records.length > 0) {
+            records.forEach(({ key, reponse, httpCode, delay }) => {
+              if (delay) {
+                sleep(delay);
+              }
+              txt = JSON.stringify(reponse);
+              status = httpCode;
+            });
+          }
+        }
+
+        if (txt !== undefined) {
+          const stream = new ReadableStream({
+            start(controller) {
+              controller.enqueue(new TextEncoder().encode(txt));
+              controller.close();
+            }
+          });
+
+          const newResponse = new Response(stream, {
+            headers: response.headers,
+            status: status,
+            statusText: status,
+          });
+          const proxy = new Proxy(newResponse, {
+            get: function (target, name) {
+              switch (name) {
+                case 'ok':
+                case 'redirected':
+                case 'type':
+                case 'url':
+                case 'useFinalURL':
+                case 'body':
+                case 'bodyUsed':
+                  return response[name];
+              }
+              return target[name];
+            }
+          });
+
+          for (let key in proxy) {
+            if (typeof proxy[key] === 'function') {
+              proxy[key] = proxy[key].bind(newResponse);
+            }
+          }
+
+          return proxy;
+        } else {
+          return response;
+        }
+      });
+    },
+
+  };
+
+  function removeURLPortAndProtocol(url: string) {
+  let ret = '';
+  try {
+    let parseURL = new URL(url);
+    parseURL.port = '';
+    ret = parseURL.toString();
+    ret = ret.replace(/^https?:\/\//, '')
+  } catch (e) {
+    ret = url;
+  }
+  return ret;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  (window as any).XMLHttpRequest = mockHttp.mockXHR;
+  (window as any).fetch = mockHttp.mockFetch;
+});
+
 
   }
 })['tuello']();
