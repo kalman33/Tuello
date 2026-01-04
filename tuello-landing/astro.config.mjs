145a0a0f2d5dcb2fabf8fcc897445b51cf4626f4
matMatthews <amathieu64@gmail.com>
2026-01-05 00:52:18 +0100

    Suppression repertoire tuello-landing

--- tuello-landing/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://tuello.dev',
-  compressHTML: true,
-  build: {
-    inlineStylesheets: 'auto'
-  }
-});
